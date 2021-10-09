db9651af9b9a6683b07e91cc886a6413f0e18d9a
Salahuddin Shaikh <salahuddin.shaikh@technokeens.com>
2021-10-09 14:21:11 +0530

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,24 @@
+
+class Home:
+
+    def __init__(self):
+        print('Random Song')
+    
+    def LivingRoom(self):
+        print('Living Room')
+    #methods 
+
+
+class FirstFloor(Home):
+    def Gym(self):
+        print('Gym')
+
+    #methods 
+    def Library(self):
+        print('Library')
+
+
+class GroundFloor(Home):
+    #methods 
+    def MasterBedRoom(self):
+        print('Master Bed Room')
